f90edada51222a5df3f241e58aea3c0f362c6f65
matteobern92@gmail.com <matteobern92@gmail.com>
2020-12-27 20:45:49 +0100

    add try catch

--- scanFolderToFile/formPrincipale.cs
+++ scanFolderToFile/formPrincipale.cs
@@ -22,117 +22,139 @@ namespace scanFolderToFile
 
         private void creaCartella()
         {
-            if (!(Directory.Exists(@"C:\CONTENT SELECTED")))
+            try
             {
-                Directory.CreateDirectory(@"C:\CONTENT SELECTED");
+                if (!(Directory.Exists(@"C:\CONTENT SELECTED")))
+                    Directory.CreateDirectory(@"C:\CONTENT SELECTED");
             }
-
+            catch (Exception) { }
         }
 
         //PULSANTE SFOGLIA
         private void button1_Click(object sender, EventArgs e)
         {
-           // MessageBox.Show("Non selezionare una cartella dove al suo interno sono presenti sottocartelle!","ATTENZIONE",MessageBoxButtons.OK,MessageBoxIcon.Exclamation);
-            MessageBox.Show("Verra' creata la cartella 'CONTENT SELECTED' in C: con il file 'CONTENUTO.TXT'", "ATTENZIONE", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
-            DialogResult result = folderBrowserDialog1.ShowDialog();
-            if (result == DialogResult.OK)
+            try
             {
-                creaCartella();
-                string[] pathFiles = Directory.GetFiles(folderBrowserDialog1.SelectedPath);
-                textBox1.Text = folderBrowserDialog1.SelectedPath;
+                MessageBox.Show("Verra' creata la cartella 'CONTENT SELECTED' in C: con il file 'CONTENUTO.TXT'", "ATTENZIONE", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
-                MessageBox.Show("Files trovati: " + pathFiles.Length.ToString(), "FILES TROVATI", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
-                StreamWriter file = new StreamWriter(@"C:\\CONTENT SELECTED\\CONTENUTO.txt");
-
-                foreach (string nomeFile in Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.*", SearchOption.AllDirectories))
+                DialogResult result = folderBrowserDialog1.ShowDialog();
+                if (result == DialogResult.OK)
                 {
-                    file.WriteLine(Path.GetFileName(nomeFile));
+                    creaCartella();
+                    string[] pathFiles = Directory.GetFiles(folderBrowserDialog1.SelectedPath);
+                    textBox1.Text = folderBrowserDialog1.SelectedPath;
+
+                    MessageBox.Show("Files trovati: " + pathFiles.Length.ToString(), "FILES TROVATI", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    StreamWriter file = new StreamWriter(@"C:\\CONTENT SELECTED\\CONTENUTO.txt");
+
+                    foreach (string nomeFile in Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.*", SearchOption.AllDirectories))
+                    {
+                        file.WriteLine(Path.GetFileName(nomeFile));
+                    }
+
+                    file.Close();
+
                 }
-
-                file.Close();
-
             }
+            catch (Exception) { }
         }
 
         //PULSANTE APERTURA FILE
         private void button2_Click(object sender, EventArgs e)
         {
-            string path = "C:\\CONTENT SELECTED\\CONTENUTO.txt";
+            try
+            {
+                string path = "C:\\CONTENT SELECTED\\CONTENUTO.txt";
 
-            if (File.Exists(path))
-            {
-                ProcessStartInfo psi;
-                psi = new ProcessStartInfo(@"C:\\CONTENT SELECTED\\CONTENUTO.txt");
-                psi.UseShellExecute = true;
-                Process.Start(psi);
+                if (File.Exists(path))
+                {
+                    ProcessStartInfo psi;
+                    psi = new ProcessStartInfo(@"C:\\CONTENT SELECTED\\CONTENUTO.txt");
+                    psi.UseShellExecute = true;
+                    Process.Start(psi);
+                }
+                else
+                {
+                    MessageBox.Show("File non ancora creato.", "FILE NON PRESENTE", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
             }
-            else
-            {
-                MessageBox.Show("File non ancora creato.", "FILE NON PRESENTE", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
+            catch (Exception) { }
         }
 
         //PULSANTE APERTURA CARTELLA
         private void button4_Click(object sender, EventArgs e)
         {
-            string path = "C:\\CONTENT SELECTED";
-            if (Directory.Exists(path))
+            try
             {
-                ProcessStartInfo psi;
-                psi = new ProcessStartInfo(@"C:\\CONTENT SELECTED");
-                psi.UseShellExecute = true;
-                Process.Start(psi);
+                string path = "C:\\CONTENT SELECTED";
+                if (Directory.Exists(path))
+                {
+                    ProcessStartInfo psi;
+                    psi = new ProcessStartInfo(@"C:\\CONTENT SELECTED");
+                    psi.UseShellExecute = true;
+                    Process.Start(psi);
+                }
+                else
+                {
+                    MessageBox.Show("Cartella non ancora creata.", "CARTELLA NON PRESENTE", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
             }
-            else
-            {
-                MessageBox.Show("Cartella non ancora creata.","CARTELLA NON PRESENTE",MessageBoxButtons.OK,MessageBoxIcon.Information);
-            }
+            catch (Exception) { }
         }
 
         //CARICA STAMPANTE DI DEFAULT
         private string GetDefaultPrinterName()
         {
-            String pkInstalledPrinters;
-            for (int i = 0; i < PrinterSettings.InstalledPrinters.Count; i++)
+            try
             {
-                pkInstalledPrinters = PrinterSettings.InstalledPrinters[i];
-                PrinterSettings settings = new PrinterSettings();
-                settings.PrinterName = pkInstalledPrinters;
-                if (settings.IsDefaultPrinter) return pkInstalledPrinters;
+                String pkInstalledPrinters;
+                for (int i = 0; i < PrinterSettings.InstalledPrinters.Count; i++)
+                {
+                    pkInstalledPrinters = PrinterSettings.InstalledPrinters[i];
+                    PrinterSettings settings = new PrinterSettings();
+                    settings.PrinterName = pkInstalledPrinters;
+                    if (settings.IsDefaultPrinter) return pkInstalledPrinters;
+                }
+                return "";
             }
-            return "";
+            catch (Exception)
+            {
+                return "";
+            }
         }
 
         //PULSANTE STAMPA FILE
         private void button3_Click(object sender, EventArgs e)
         {
-            string path = "C:\\CONTENT SELECTED\\CONTENUTO.txt";
-            if (File.Exists(path))
+            try
             {
-                ProcessStartInfo psi = new ProcessStartInfo(@"C:\\CONTENT SELECTED\\CONTENUTO.txt");
-                psi.Verb = "print";
-                try
+                string path = "C:\\CONTENT SELECTED\\CONTENUTO.txt";
+                if (File.Exists(path))
                 {
-                    psi.Arguments = GetDefaultPrinterName();
-                    if (!(psi.Arguments == ""))
+                    ProcessStartInfo psi = new ProcessStartInfo(@"C:\\CONTENT SELECTED\\CONTENUTO.txt");
+                    psi.Verb = "print";
+                    try
                     {
-                        Process print = new Process();
-                        print.StartInfo = psi;
-                        print.Start();
+                        psi.Arguments = GetDefaultPrinterName();
+                        if (!(psi.Arguments == ""))
+                        {
+                            Process print = new Process();
+                            print.StartInfo = psi;
+                            print.Start();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Si sono verificati problemi per la stampa : " + ex.Message);
                     }
                 }
-                catch (Exception ex)
+                else
                 {
-                    MessageBox.Show("Si sono verificati problemi per la stampa : " + ex.Message);
+                    MessageBox.Show("File non ancora creato.", "FILE NON PRESENTE", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
             }
-            else
-            {
-                MessageBox.Show("File non ancora creato.", "FILE NON PRESENTE", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
+            catch (Exception) { }
         }
     }
 }
-  
