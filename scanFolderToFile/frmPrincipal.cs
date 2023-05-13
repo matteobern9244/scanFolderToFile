f78a2198c21b4e977d92bff1d28a6025b90b61c1
Matteo Bernardini <matteobern92@gmail.com>
2023-05-13 19:34:26 +0200

    wip class Utils

--- scanFolderToFile/frmPrincipal.cs
+++ scanFolderToFile/frmPrincipal.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Drawing.Printing;
 using System.IO;
 using System.Linq;
 using System.Windows.Forms;
-using Spire.Pdf;
-using Spire.Pdf.Graphics;
-using Spire.Pdf.Lists;
 using static ScanFolderToFile.Constants;
 using static ScanFolderToFile.Utils.Utils;
 
@@ -50,81 +45,6 @@ namespace ScanFolderToFile
             catch (Exception) { }
         }
 
-        private void CreateFilePdf(bool onlyExtension, List<string> content)
-        {
-            var doc = new PdfDocument();
-
-            //Set the margins
-            var margins = new PdfMargins(30);
-
-            //Add a page
-            var page = doc.Pages.Add(PdfPageSize.A4, margins);
-
-            //Create a brush
-            var brush = PdfBrushes.Black;
-
-            //Create two fonts
-            var titleFont = new PdfFont(PdfFontFamily.TimesRoman, 12f, PdfFontStyle.Bold);
-            var listFont = new PdfFont(PdfFontFamily.TimesRoman, 12f, PdfFontStyle.Regular);
-
-            //Specify the initial coordinate
-            float x = 0;
-            float y = 0;
-
-            //Draw title
-            var title = "CONTENT";
-            page.Canvas.DrawString(title, titleFont, brush, x, y);
-            y = y + (float)titleFont.MeasureString(title).Height;
-            y = y + 5;
-
-            var list = new PdfSortedList(string.Join(Environment.NewLine, content))
-            {
-                //Set the font, indent, text indent, brush of the list
-                Font = listFont,
-                Indent = 2,
-                TextIndent = 4,
-                Brush = brush
-            };
-
-            //Draw list on the page at the specified location
-            list.Draw(page, 0, y);
-
-            //Save to file
-            doc.SaveToFile(Path.Combine(PathFolder, PdfFileFinal));
-        }
-
-        private void CreateFileTxt(bool onlyExtension, List<string> content)
-        {
-            var file = new StreamWriter(Path.Combine(PathFolder, TxtFileFinal));
-
-            var listElement = new List<string>();
-
-            if (content.Any())
-            {
-                foreach (var sFile in content)
-                {
-                    if (onlyExtension)
-                    {
-                        var extension = Path.GetExtension(sFile).Trim();
-                        if (!listElement.Contains(extension))
-                            listElement.Add(extension);
-                    }
-                    else
-                        file.WriteLine(Path.GetFileName(sFile));
-                }
-
-                if (listElement.Any())
-                {
-                    foreach (var extension in listElement)
-                    {
-                        file.WriteLine(extension);
-                    }
-                }
-            }
-
-            file.Close();
-        }
-
         //PULSANTE APERTURA FILE
         private void btnOpenFile_Click(object sender, EventArgs e)
         {
@@ -165,29 +85,6 @@ namespace ScanFolderToFile
                 }
             }
             catch (Exception) { }
-        }
-
-        //CARICA STAMPANTE DI DEFAULT
-        private static string GetDefaultPrinterName()
-        {
-            try
-            {
-                for (var i = 0; i < PrinterSettings.InstalledPrinters.Count; i++)
-                {
-                    var pkInstalledPrinters = PrinterSettings.InstalledPrinters[i];
-                    var settings = new PrinterSettings
-                    {
-                        PrinterName = pkInstalledPrinters
-                    };
-                    if (settings.IsDefaultPrinter)
-                        return pkInstalledPrinters;
-                }
-                return string.Empty;
-            }
-            catch (Exception)
-            {
-                return string.Empty;
-            }
         }
 
         //PULSANTE STAMPA FILE
