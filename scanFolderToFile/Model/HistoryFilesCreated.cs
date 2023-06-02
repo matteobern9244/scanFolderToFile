249c6bb5c990ee7d15625a860d056fd2a646088f
Matteo Bernardini <matteobern92@gmail.com>
2023-06-02 20:51:41 +0200

    Wip

---
+++ scanFolderToFile/Model/HistoryFilesCreated.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ScanFolderToFile.Model
+{
+    public class HistoryFilesCreated
+    {
+        public class FileCreated
+        {
+            public string NameFile { get; set; }
+            public string ExtensionFile { get; set; }
+            public DateTime CreatedAt { get; set; }
+        }
+    }
+}
